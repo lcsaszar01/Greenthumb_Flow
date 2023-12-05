5833760840e3f084d53c9e9cbc5d9e71d4acf582
kpmealey <kmealey2@nd.edu>
2023-12-05 12:37:44 -0500

    fixed to create times

--- database/make_csvs.py
+++ database/make_csvs.py
@@ -5,7 +5,9 @@ import random
 import json
 import time
 
-time_str= time.asctime(time.localtime(time.time())).replace(" ","--")
+time_15 = [stamp.strftime('Tue--Dec----5--%H:%M:%S') for stamp in pd.date_range(start='2023-12-05 00:00:00', end='2023-12-05 23:45:00', freq='15T')]
+time_30 = [stamp.strftime('Tue--Dec----5--%H:%M:%S') for stamp in pd.date_range(start='2023-12-05 00:00:00', end='2023-12-05 23:45:00', freq='30T')]
+time_60 = [stamp.strftime('Tue--Dec----5--%H:%M:%S') for stamp in pd.date_range(start='2023-12-05 00:00:00', end='2023-12-05 23:45:00', freq='60T')]
 
 with open('weather_descriptions.json', 'r') as file:
     data = json.load(file)
@@ -15,20 +17,20 @@ def generate_random(n, range_min, range_max):
     return [random.uniform(range_min, range_max) for _ in range(n)]
 
 #sensor_df = pd.DataFrame(columns=["time", "zone", "moisture", "humidity", "temperature", "ir", "vis", "uv", "ph"])
-n = 24*4
-sensor_data = {"time":[time_str]*n*2, "zone":["A", "B"]*n, "moisture":generate_random(n*2,0,100), "humidity":generate_random(n*2,0,100), "temperature":generate_random(n*2, 40, 90),"ir":generate_random(n*2, 0,100), "vis":generate_random(n*2, 0, 100), "uv":generate_random(n*2, 0, 100), "ph":generate_random(n*2, 5, 8)}
+n = len(time_15)
+sensor_data = {"time":sorted(time_15*2), "zone":["A", "B"]*n, "moisture":generate_random(n*2,0,100), "humidity":generate_random(n*2,0,100), "temperature":generate_random(n*2, 40, 90),"ir":generate_random(n*2, 0,100), "vis":generate_random(n*2, 0, 100), "uv":generate_random(n*2, 0, 100), "ph":generate_random(n*2, 5, 8)}
 sensor_df = pd.DataFrame(sensor_data)
 sensor_df.to_csv('sensors.csv', index=False)
 
 #watering_df = pd.DataFrame(columns=["time", "zone", "amount (L)"])
-m = 24
-watering_data = {"time":[time_str]*m*2, "zone":["A", "B"]*m, "amount (L)":generate_random(m*2, 0, 500)}
+m = len(time_60)
+watering_data = {"time":sorted(time_60*2), "zone":["A", "B"]*m, "amount (L)":generate_random(m*2, 0, 500)}
 watering_df = pd.DataFrame(watering_data)
 watering_df.to_csv('watering.csv', index=False)
 
 
 #weather_df = pd.DataFrame(columns=["time","temperature", "weather"])
-p = 24*2
-weather_data = {"time":[time_str]*p, "temperature":generate_random(p,40,90), "weather":random.choices(weather_descriptions, k=p)}
+p = len(time_30)
+weather_data = {"time":time_30, "temperature":generate_random(p,40,90), "weather":random.choices(weather_descriptions, k=p)}
 weather_df = pd.DataFrame(weather_data)
 weather_df.to_csv('weather.csv', index=False)
