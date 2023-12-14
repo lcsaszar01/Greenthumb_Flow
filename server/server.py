ddd9fa78e92314e83f3dc10fdfff382580a02e9f
hannahlong7 <hlong3@nd.edi>
2023-12-13 20:21:22 -0500

    matplotlib.pyplot edits - datafame and first chart created!

--- server/server.py
+++ server/server.py
@@ -2,6 +2,8 @@
 
 
 from flask import Flask, render_template, request
+import numpy as np 
+import matplotlib.pyplot as plt 
 from weather import forecast
 from markupsafe import escape
 from markupsafe import Markup
@@ -12,7 +14,8 @@ import json
 from celery import Celery, Task
 from celery.schedules import crontab
 import tasks
-4
+import os
+
 app = Flask(__name__)
 app.config.from_pyfile('celeryconfig.py')
 
@@ -21,6 +24,45 @@ app.config.from_pyfile('celeryconfig.py')
 def index(name=None):
     return render_template('index.html') 
 # task_id=result.id)
+
+def get_plot(): 
+    df = pd.read_csv("C:/Users/longf/Greenthumb_Flow/database/sensors.csv")
+    df = df[df["zone"].isin(["A"])]
+    df = df.drop(columns=['time'])
+
+    # data = { 
+    #     'a': np.arange(50), 
+    #     'c': np.random.randint(0, 50, 50), 
+    #     'd': np.random.randn(50) 
+    # } 
+    # data['b'] = data['a'] + 10 * np.random.randn(50) 
+    # data['d'] = np.abs(data['d']) * 100
+  
+    # df.plot.scatter('a', 'b', c='c', s='d', data=data) 
+
+    # ax1 = df.plot(kind='scatter', x='moisture', y='humidity', color='r')
+    # ax2 = df.plot(kind='scatter', x='moisture', y='humidity', color='r')
+    # df.plot.xlabel('X label') 
+    # plot.ylabel('Y label') 
+    ax = df.plot(kind="scatter", x="moisture",y="humidity", color="b", label="moisture vs. humidity")
+    df.plot(x="temperature",y="humidity", color="r", label="temp. vs. humidity", ax=ax)
+    df.plot( x="moisture",y="ph", color="g", label="moisture vs. ph", ax=ax)
+    df.plot( x="temperature",y="uv", color="orange", label="temp. vs uv", ax=ax)
+    df.plot( x="humidity",y="ph", color="purple", label="humidity vs. ph", ax=ax)
+    ax.set_xlabel("horizontal label")
+    ax.set_ylabel("vertical label")
+    return plt 
+
+@app.get('/data') 
+def plot_data(): 
+    # Get the matplotlib plot  
+    plot = get_plot() 
+
+    # Save the figure in the static directory 
+    # CHANGE TO YOUR ABSOLUTE PATH BEFORE RUNNING 
+    plot.savefig("c:/Users/longf/Greenthumb_Flow/server/static/plot.png") 
+  
+    return render_template('matplotlib-plot1.html') 
 
 @app.route('/more')
 def more(name=None):
